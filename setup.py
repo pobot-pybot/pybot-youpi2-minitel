24c395dc8290e9a672524f9d109efd8dc39a9639
Eric Pascual <eric@pobot.org>
2016-09-24 12:36:39 +0200

    package renamed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pybot-youpi2-minitel',
     setup_requires=['setuptools_scm'],
     use_scm_version={
-        'write_to': 'src/pybot/youpi2nitel/__version__.py'
+        'write_to': 'src/pybot/youpi2/minitel/__version__.py'
     },
     namespace_packages=['pybot'],
     packages=find_packages("src"),
@@ -13,9 +13,9 @@ setup(
     license='',
     author='Eric Pascual',
     author_email='eric@pobot.org',
-    install_requires=['pybot-core', 'pybot-dspin', 'pybot-youpi2'],
+    install_requires=['pybot-core', 'pybot-dspin', 'pybot-youpi2', 'pybot-minitel'],
     download_url='https://github.com/Pobot/PyBot',
-    description='Library for Youpi arm controlled by STMicro L6470 (aka dSPIN)',
+    description='Youpi2 Minitel control UI',
     entry_points={
         'console_scripts': [
             'youpi2-minitel = pybot.youpi2nitel.toplevel:main'
